2338b117c1844952e8e9b295c7488268da92a64b
ahmedsamir9924 <ahmedsamir9924@gmail.com>
2023-12-05 14:37:21 +0200

    add systemcar table

--- IntelligentDiagnostics.API/Program.cs
+++ IntelligentDiagnostics.API/Program.cs
@@ -1,4 +1,9 @@
 
+using IntelligentDiagnostics.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
 namespace IntelligentDiagnostics.API
 {
     public class Program
@@ -8,12 +13,18 @@ namespace IntelligentDiagnostics.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var configuration = new ConfigurationBuilder()
+             .AddJsonFile("appsettings.json") 
+             .Build();
+            //add connectionString 
+            builder.Services.AddDbContext<AppDbContext>(builder => builder.UseSqlServer(configuration.GetConnectionString("DefaultConnectionStrings"),
+                        b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
 
             builder.Services.AddControllers();
+            
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
---
+++ IntelligentDiagnostics.Core/Models/SystemCar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelligentDiagnostics.Core.Models
+{
+    public class SystemCar
+    {
+        public int SystemCarId { get; set; }
+        public string SystemCarName { get; set; }
+    }
+}
