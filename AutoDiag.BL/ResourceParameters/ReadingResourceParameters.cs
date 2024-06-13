577b062878e5fb118a9641ff7bdb2cd6163c2028
Eileanora <yasmeenhany14@gmail.com>
2024-06-13 14:14:29 +0300

    add filter by date to readings

--- AutoDiag.BL/ResourceParameters/ReadingResourceParameters.cs
+++ AutoDiag.BL/ResourceParameters/ReadingResourceParameters.cs
@@ -2,9 +2,19 @@
 
 public class ReadingResourceParameters : BaseResourceParameters
 {
+    private int _pageSize = 50;
+    private const int MaxPageSize = 100;
+
     public ReadingResourceParameters()
     {
         OrderBy = "CreatedDate desc";
     }
 
+    public string? CreatedDate { get; set; }
+
+    public override int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
 }
